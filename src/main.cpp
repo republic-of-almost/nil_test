91bd7f6a88bb3c9d9de9ccc0ab1d881d918371ff
Phil Cooper-King <phil@cooperking.net>
2017-05-07 22:12:20 +0100

    Test changes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <nil/nil.hpp>
 #include <nil/node.hpp>
-#include <nil/data/window.hpp>
+#include <nil/data/data.hpp>
+
+#include <external/GL/gl3w.h>
 
 #define SDL_ASPECT_IMPL
 #include <sdl_aspect.hpp>
 
 #define IMGUI_ASPECT_IMPL
 #include <imgui_aspect.hpp>
+
+#define RENDERER_ASPECT_IMPL
+#include <renderer_aspect.hpp>
+
+
+#define ROV_GL_IMPL
+#include <external/rov/rov.hpp>
 
 #include <assert.h>
 
@@ -21,31 +30,52 @@ main()
   Nil_ext::ImGUI_Aspect imgui_aspect;
   nil_engine.add_aspect(&imgui_aspect);
   
+  Nil_ext::GL_Aspect gl_aspect;
+  nil_engine.add_aspect(&gl_aspect);
+  
   Nil::Node context;
-  context.set_name("ctx");
-  
-  Nil::Node input;
-  input.set_name("input");
-  input.set_parent(context);
-  
-  Nil::Node keyboard_mouse;
-  keyboard_mouse.set_name("kb-mouse");
-  keyboard_mouse.set_parent(input);
-  
-  
-  context.set_name("ctx");
   {
+    context.set_name("Ctx");
+    
     Nil::Data::Window window{};
     window.width = 0;
     window.height = 0;
     window.fullscreen = false;
     
-    Nil::Data::get(context, window);
-    
     Nil::Data::set(context, window);
   }
   
-  while(nil_engine.run());
+  Nil::Node ground;
+  {
+    ground.set_name("Ground");
+    
+    Nil::Data::Material mat{};
+    
+    Nil::Data::set(ground, mat);
+  }
+  
+  Nil::Node camera;
+  {
+    camera.set_name("Camera");
+    
+    Nil::Data::Camera cam{};
+    cam.width = 1200;
+    cam.height = 720;
+    cam.fov = 0.4f;
+    cam.near_plane = 0.1f;
+    cam.far_plane = 100.f;
+    cam.type = Nil::Data::Camera::PERSPECTIVE;
+    cam.clear_color_buffer = true;
+    cam.clear_depth_buffer = true;
+    
+    Nil::Data::set(camera, cam);
+  }
+  
+  
+  while(nil_engine.run())
+  {
+
+  }
   
   
   return 0;
